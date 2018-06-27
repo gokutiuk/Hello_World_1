1d6fe6843714b0cf2f8029883fe42adca275931b
Oleg <gokutiuk@ukr.net>
2018-06-27 10:30:43 +0300

    Initial commit

---
+++ src/main/java/Hello_World.java
@@ -0,0 +1,18 @@
+public class Hello_World
+{
+    public static void main (String [] args)
+    {
+        System.out.println("hello world");
+
+    }
+    public  static  void printMsg ( String s)
+    {
+        System.out.println(s);
+    }
+    public static int plus ( int a , int b)
+    {
+        a=5; b = 10;
+        int  res = a + b;
+        return res;
+    }
+}
